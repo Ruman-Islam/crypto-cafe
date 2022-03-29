0c0c2e0d6f30d195c1c0475fd4fdc9bd30b1c7b2
Ruman-Islam <rumanislam48@gmail.com>
2022-03-30 04:29:15 +0600

    brand logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
+import logo from '../../logo.png'
 
 const Header = () => {
     return (
         <div className='sticky top-0 bg-white px-12 py-8 flex justify-center md:justify-between'>
-            <div className='hidden md:block'>
-                <Link to='/' className='font-sans font-bold text-xl text-gray-600'>Crypto Cafe</Link>
+            <div className='hidden md:block w-44'>
+                <Link to='/' className='flex items-center justify-between font-sans font-bold text-2xl text-gray-600'>
+                    <h1>Crypto Cafe</h1>
+                    <span className='text-2xl'>☕</span>
+                </Link>
             </div>
             <div className='flex gap-4 text-xl'>
                 <CustomLink to='/'>Home</CustomLink>
