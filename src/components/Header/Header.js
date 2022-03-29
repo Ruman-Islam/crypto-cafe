8eb1c3ac68aa94697db17b05981f29b7edf2929d
Ruman-Islam <rumanislam48@gmail.com>
2022-03-30 04:35:10 +0600

    readme added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
-import logo from '../../logo.png'
 
 const Header = () => {
     return (
